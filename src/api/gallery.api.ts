13568918175e27b7f563861c938ad4aa02d36d30
n-a-n-a-k-i <nanaki.god@gmail.com>
2021-10-19 16:50:16 +0300

    update gallery api response interceptor

--- src/api/gallery.api.ts
+++ src/api/gallery.api.ts
@@ -1,17 +1,42 @@
 import axios from "axios";
+import AccountService from "../service/account.service";
 
 const galleryApi = axios.create({
     baseURL: process.env.REACT_APP_GALLERY_SERVER_URL,
     withCredentials: true
 })
 
-galleryApi.interceptors.request.use(config => {
+galleryApi.interceptors.request.use((config) => {
 
     config.headers = {
         Authorization: `Bearer ${localStorage.getItem('accessToken')}`
     }
 
     return config
+
+})
+
+galleryApi.interceptors.response.use((config) => config, async (error) => {
+
+    if (error.response.status === 401) {
+
+        try {
+
+            const response = await AccountService.refresh()
+
+            localStorage.setItem('accessToken', response.data.accessToken)
+
+            return galleryApi.request(error.config)
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+
+    }
+
+    throw error
 
 })
 
